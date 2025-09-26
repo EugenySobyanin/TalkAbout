79a73aae9ff027aa238b259d28faf775e922b0ee
Евгений Собянин <ea.sobyanin2023@gmail.com>
2025-09-26 21:48:12 +0300

    Модели для приложения activities.

--- backend/activities/models.py
+++ backend/activities/models.py
@@ -27,32 +27,46 @@ class UserFilmActivities(models.Model):
         verbose_name='Фильм',
     )
     is_planned = models.BooleanField('Буду смотерь', default=False)
-    is_wathed = models.BooleanField('Посмотрел', default=False)
+    is_wathed = models.BooleanField('Просмотрено', default=False)
     rating = models.SmallIntegerField(
         'Оценка',
         null=True,
         blank=True,
-        validators= [
+        validators=[
             MinValueValidator(0, 'Миинмальная балл 0.'),
             MaxValueValidator(10, 'Максимальный балл 10.')
         ]
     )
     is_public = models.BooleanField('Публичный', default=False)
-    # Остановился здесь __________________________________________________________________________________________________________________________
-    last_watched_date = models.DateTimeField()
-    created_at = models.DateField()
-    updated_at = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
 
 
 class HistoryWatching(models.Model):
     """История просмотров пользователей."""
 
+    user_film_activities = models.ForeignKey(
+        UserFilmActivities,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь-Фильм',
+        related_name='watching_history',
+    )
+    watched_date = models.DateTimeField(auto_now_add=True)
+    comment = models.CharField(
+        'Комментарий к просмотру',
+        max_length=1000,
+        null=True,
+        blank=True
+    )
+
 
 class Collections(models.Model):
-    """Подборки.
-
-    Сборники с фильмами.
     """
+    Подборки/Коллекции с фильмами.
+
+    Подборки могут создавать и пользоватали и админы.
+    """
+    # Остановился здесь_________________________________________________________________________________
 
 
 class CollectionFilms(models.Model):
