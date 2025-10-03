42bd18b66b5c663a5f99864b3fa667f180de60d2
Евгений Собянин <ea.sobyanin2023@gmail.com>
2025-10-03 19:55:13 +0300

    ничего

--- backend/activities/views.py
+++ backend/activities/views.py
@@ -18,6 +18,7 @@ def add_user_activitie(request, film_id):
     if request.method == 'POST':
         film = get_object_or_404(Film, pk=film_id)
         data = request.data.copy()
+        print(data)
         data['user'] = request.user.pk
         data['film'] = film.pk
 
