225f012d4df09be96c9bbbe07c9ff0c320876456
Евгений Собянин <ea.sobyanin2023@gmail.com>
2025-10-05 22:16:20 +0300

    views для activities

--- backend/activities/serializers.py
+++ backend/activities/serializers.py
@@ -4,10 +4,11 @@ from activities.models import UserFilmActivitie
 from gallery.models import Film
 
 
-class UserActivitieSerializer(serializers.ModelSerializer):
+class AddUserActivitieSerializer(serializers.ModelSerializer):
     """Сериализатор для фильмов."""
 
     class Meta:
         model = UserFilmActivitie
-        fields = ('id', 'user', 'film', 'is_planned', 'is_watched', 'rating', 'is_public',)
-        read_only_fields = ('is_watched', 'rating', 'is_public')
+        fields = ('id', 'user', 'film',
+                  'is_planned', 'is_watched', 'rating', 'is_public')
+        read_only_fields = ('rating', 'is_public')
