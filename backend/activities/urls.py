2832cc66f192a0378defea1980a7d6d69a108f3e
ea.sobyanin2018@gmail.com <PC1013487026@DTDF.ru>
2026-02-09 18:47:47 +0300

    Небольшие правки.

--- backend/activities/urls.py
+++ backend/activities/urls.py
@@ -5,12 +5,8 @@ from activities.views import UserActivitiesView, UserFilmActivityView
 
 film_router = routers.DefaultRouter()
 
-# film_router.register('films', UserActivitieViewSet, basename='films')
 
 urlpatterns = [
-    # path('', include(film_router.urls)),
-    # path('user_activities/<int:film_id>/', UserFilmActivitieView.as_view()),
     path('user_activities/', UserActivitiesView.as_view()),
     path('user_activities/<int:film_id>/', UserFilmActivityView.as_view()),
-    
 ]
--- backend/activities/views.py
+++ backend/activities/views.py
@@ -8,76 +8,11 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
 from activities.models import UserFilmActivitie, HistoryWatching
-from activities.serializers import AddUserActivitieSerializer
+from activities.serializers import AddUserActivitySerializer
 from gallery.models import Film
 
 
 User = get_user_model()
-
-
-# class UserActivitieViewSet(viewsets.ModelViewSet):
-#     """Вьюсет для 'Просмотренных' и 'Планируемых'."""
-
-
-# @api_view(['POST', 'GET'])
-# def add_user_activitie(request, film_id):
-#     if request.method == 'POST':
-#         film = get_object_or_404(Film, pk=film_id)
-#         data = request.data.copy()
-#         data['user'] = request.user.pk
-#         data['film'] = film.pk
-
-#         user_activitie = UserFilmActivitie.objects.filter(
-#             user=request.user, film=film
-#         ).first()
-
-#         # Нельзя добавлять фильм в "Планируемые" и в "Просмотренные" одним запросом. ___________ Нужно ли вообще это ограничение?
-#         if 'is_planned' in data and 'is_watched' in data:
-#             return Response(
-#                 {'error': 'Нельзя одним запросом добавлять '
-#                  'в "Планируемые" и в "Просмотренные".'},
-#                 status=status.HTTP_400_BAD_REQUEST
-#             )
-
-#         # Если у пользователя нет записи UserActivitie с этим фильмом
-#         if not user_activitie:
-#             serializer = AddUserActivitieSerializer(data=data)
-
-#             if serializer.is_valid():
-#                 serializer.save()
-#                 return Response(serializer.data,
-#                                 status=status.HTTP_201_CREATED)
-#             else:
-#                 return Response(serializer.errors,
-#                                 status=status.HTTP_400_BAD_REQUEST)
-
-#         # Когда запись UserActivitie существует
-#         else:
-#             if user_activitie.is_planned and 'is_planned' in data:
-#                 return Response({'error': 'Фильм уже добавлен в "Планируемые".'})  # ___________________________Нужно ли вообще это ограничение? Ну если отпавляется запрос еще раз, ну и пусть отправляется
-#             else:
-#                 serializer = AddUserActivitieSerializer(user_activitie, data=data, partial=True)
-#                 if serializer.is_valid():
-#                     serializer.save()
-#                     return Response(serializer.data, status=status.HTTP_201_CREATED)
-#                 else:
-#                     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-                
-#     # # Когда GET запрос_________Зачем нам здесь вообще GET запрос???????
-#     # else:
-#     #     if not user_id:  # Если user_id пуст, значит надо вернуть список фильмов текущего пользователя
-#     #         user_activities = UserFilmActivitie.objects.filter(user=request.user)
-#     #         serializer = AddUserActivitieSerializer(user_activities, many=True)
-#     #         if serializer.is_valid():
-#     #             serializer.save()
-#     #             return Response(serializer.data, status=status.HTTP_200_OK)
-#     #         else:
-#     #             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(['GET'])
-# def get_user_activities(request, user_id=None, ):
-#     pass
 
 
 class UserActivitiesView(APIView):
@@ -109,7 +44,7 @@ class UserActivitiesView(APIView):
         # Сортировка по дате добавления
         queryset = queryset.order_by('-created_at')
 
-        serializer = AddUserActivitieSerializer(queryset, many=True)
+        serializer = AddUserActivitySerializer(queryset, many=True)
         return Response(serializer.data)
 
 
@@ -149,7 +84,7 @@ class UserFilmActivityView(APIView):
                'is_watched' in data and activity.is_watched == data['is_watched']:
                 # Просто обновляем другие поля (если есть)
                 if len(data) > 2:  # Если есть поля кроме user и film
-                    serializer = AddUserActivitieSerializer(
+                    serializer = AddUserActivitySerializer(
                         activity, 
                         data=data, 
                         partial=True
@@ -159,16 +94,16 @@ class UserFilmActivityView(APIView):
                         return Response(serializer.data, status=status.HTTP_200_OK)
                 else:
                     # Ничего не меняется, возвращаем текущее состояние
-                    serializer = AddUserActivitieSerializer(activity)
+                    serializer = AddUserActivitySerializer(activity)
                     return Response(serializer.data, status=status.HTTP_200_OK)
             
             # Обновляем существующую запись
-            serializer = AddUserActivitieSerializer(activity, data=data, partial=True)
+            serializer = AddUserActivitySerializer(activity, data=data, partial=True)
             status_code = status.HTTP_200_OK
             
         except UserFilmActivitie.DoesNotExist:
             # Создаем новую запись
-            serializer = AddUserActivitieSerializer(data=data)
+            serializer = AddUserActivitySerializer(data=data)
             status_code = status.HTTP_201_CREATED
 
         # Валидируем и сохраняем
@@ -211,7 +146,7 @@ class UserFilmActivityView(APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        serializer = AddUserActivitieSerializer(activity, data=data, partial=True)
+        serializer = AddUserActivitySerializer(activity, data=data, partial=True)
 
         if serializer.is_valid():
             serializer.save()
