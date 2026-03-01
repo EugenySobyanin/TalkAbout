0b8026338809410cb10019b4cc06728a81c1820f
Евгений Собянин <ea.sobyanin2023@gmail.com>
2026-03-01 20:22:28 +0300

    Рефакторинг архитектуры.

--- backend/talk_about/settings.py
+++ backend/talk_about/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'gallery.apps.GalleryConfig',
     'activities.apps.ActivitiesConfig',
     'compilations.apps.CompilationsConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
