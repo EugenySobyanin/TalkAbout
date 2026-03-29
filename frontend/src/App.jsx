ca678bd98552f3e12a65107b24a94ead43d0ec60
Евгений Собянин <ea.sobyanin2023@gmail.com>
2026-03-29 22:05:39 +0300

    Разработка фронта.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Header from './components/header'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <h1>Hello, world!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <header>
+            <div className="header">
+                <h2>ЛОГОТИП!</h2>
+                <h2>ИМЯ и ФАМИЛИЯ!</h2>
+                <form>
+                    <input type="text"/>
+                </form>
+            </div>
+        </header>
+    )
+}
+
+export default Header
